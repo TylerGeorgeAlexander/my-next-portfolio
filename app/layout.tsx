e8ba51f393b3451701b1ecfaa1ea2cc0f77c01c2
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-10-10 12:04:41 -0400

    fix: - ThemeProvider creates an error when toggling day/night mode - using suppressHydrationWarning in html element can bypass

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,13 +10,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
       <body className="dark:bg-stone-900">
+        {/* ThemeProvider creates an error when toggling day/night mode - using suppressHydrationWarning in html element can bypass */}
         <ThemeProvider enableSystem={true} attribute="class">
           <Navbar />
           {children}
