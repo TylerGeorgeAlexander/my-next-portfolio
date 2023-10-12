997c872ed372de935d6f8711b027808849207362
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2023-10-11 21:07:24 -0400

    css: - contact form background and dark:bg set with rounded edges - half size large and medium screens

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState } from "react";
+import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
 import SlideUp from "./SlideUp";
 
 interface ContactForm {
@@ -44,45 +45,34 @@ const ContactSection: React.FC = () => {
     <section id="contact" className="relative">
       <h1 className="my-10 text-center font-bold text-4xl">
         Contact
-        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
       </h1>
       <SlideUp>
-        <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap animate-slideUpCubiBezier animation-delay-2">
-          <div className="lg:w-2/3 md:w-1/2 bg-gray-900 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-            <iframe
-              width="100%"
-              height="100%"
-              title="map"
-              className="absolute inset-0"
-              frameBorder={0}
-              marginHeight={0}
-              marginWidth={0}
-              style={{ filter: "opacity(0.7)" }}
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53106.91866391939!2d-84.41452306314744!3d33.70422841472449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88f5024659a2f351%3A0x8d59ab9473b28ba4!2sAtlanta%2C%20GA%2030315!5e0!3m2!1sen!2sus!4v1670303570825!5m2!1sen!2sus"
-            />
-            <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
-              <div className="lg:w-1/2 px-6">
-                <h2 className="title-font font-semibold text-white tracking-widest text-xs">
-                  ADDRESS
-                </h2>
-                <p className="mt-1">Atlanta, GA</p>
-              </div>
-              <div className="lg:w-1/2 px-6 mt-4 lg:mt-0"></div>
+        <div className="container rounded-lg bg-gray-300 dark:bg-gray-800 px-5 py-10 mx-auto flex flex-col md:flex-row items-center justify-center animate-slideUpCubiBezier animation-delay-2">
+          <div className="lg:w-1/2 md:w-full rounded-lg overflow-hidden sm:mr-0 p-10 flex flex-col items-center">
+            <div className="contact-method mb-6">
+              <span className="icon solid alt">
+                <AiOutlineMail />
+              </span>
+              <h3>Email</h3>
+              <a href="mailto:TylerGeorgeAlexander.dev@gmail.com">
+                TylerGeorgeAlexander.dev@gmail.com
+              </a>
+            </div>
+            <div className="contact-method">
+              <span className="icon solid alt">
+                <AiOutlinePhone />
+              </span>
+              <h3>Phone</h3>
+              <span>+56 9 5617 7204</span>
             </div>
           </div>
           <form
             name="contact"
             onSubmit={handleSubmit}
-            className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0"
+            className="lg:w-1/2 md:w-full flex flex-col text-center items-center md:ml-auto w-full md:py-8 mt-8 md:mt-0"
           >
-            <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-              Hire Me
-            </h2>
-            <p className="leading-relaxed mb-5">
-              If you are looking for a website or software engineer, please feel
-              free to reach out to me. I would love to work with you!
-            </p>
-            <div className="relative mb-4">
+            <div className="relative mb-4 w-full">
               <label htmlFor="name" className="leading-7 text-sm text-gray-400">
                 Name
               </label>
@@ -90,11 +80,11 @@ const ContactSection: React.FC = () => {
                 type="text"
                 id="name"
                 name="name"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="input-style w-full" // Change the class to w-full
                 onChange={handleChange}
               />
             </div>
-            <div className="relative mb-4">
+            <div className="relative mb-4 w-full">
               <label
                 htmlFor="email"
                 className="leading-7 text-sm text-gray-400"
@@ -105,11 +95,11 @@ const ContactSection: React.FC = () => {
                 type="email"
                 id="email"
                 name="email"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                className="input-style w-full" // Change the class to w-full
                 onChange={handleChange}
               />
             </div>
-            <div className="relative mb-4">
+            <div className="relative mb-4 w-full">
               <label
                 htmlFor="message"
                 className="leading-7 text-sm text-gray-400"
@@ -119,14 +109,11 @@ const ContactSection: React.FC = () => {
               <textarea
                 id="message"
                 name="message"
-                className="w-full bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                className="flex textarea-style w-full" // Change the class to w-full
                 onChange={handleChange}
               />
             </div>
-            <button
-              type="submit"
-              className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-            >
+            <button type="submit" className="button-style">
               Submit
             </button>
           </form>
@@ -135,4 +122,5 @@ const ContactSection: React.FC = () => {
     </section>
   );
 };
+
 export default ContactSection;
