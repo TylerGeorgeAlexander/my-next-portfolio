789e8d9e4cd39b14713dc1cb0ecacf518a9e2267
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2024-03-13 18:25:57 -0400

    Add blog feature to portfolio using Next.js Blog Starter Kit
    Implemented the blog feature inspired by the Next.js Blog Starter Kit from Vercel. This includes:
    
    - Static generation of blog posts using Markdown files
    - Dynamic routing for individual blog post pages
    - A simple blog index page to list all posts
    - SEO optimizations with meta tags for blog posts
    
    This feature enhances the portfolio by showcasing technical writing skills and industry insights, providing valuable content to visitors.

---
+++ app/posts/layout.tsx
@@ -0,0 +1,38 @@
+import Footer from "@/app/_components/footer";
+import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/app/lib/constants";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: `Next.js Blog Example with ${CMS_NAME}`,
+  description: `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  openGraph: {
+    images: [HOME_OG_IMAGE_URL],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    // TODO: clean up duplicate layout semantic html
+    // <html lang="en">
+    //   <body className={inter.className}>
+    //     <div className="min-h-screen">{children}</div>
+    //     <Footer />
+    //   </body>
+    // </html>
+    <div lang="en">
+    <div className={inter.className}>
+      <div className="min-h-screen">{children}</div>
+      <Footer />
+    </div>
+  </div>
+  );
+}
